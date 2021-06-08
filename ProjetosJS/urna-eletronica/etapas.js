5fbcb9d3d8cdf02c00aed2cb339865aa85ae444c
Tiago <tiagoalmeidasp@hotmail.com>
2021-06-08 20:15:54 -0300

    finalizando urna eletronica

--- ProjetosJS/urna-eletronica/etapas.js
+++ ProjetosJS/urna-eletronica/etapas.js
@@ -4,21 +4,21 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
+                numero: '38111',
                 nome: 'Fulano de Tal',
                 partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
                 fotos:[
                     {url:'77222.jpg', legenda: 'Vereador'}
                 ]
             },
-            {
-                nomero: 77222,
-                nome: 'Beltrano de Silva',
-                partido: 'ACJEF',
-                fotos: [
-                    {url:'77222.jpg', legenda: 'Vereador'}
-                ]
-            },            
         ]
     },
     {
@@ -26,25 +26,25 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
+                numero: '99',
                 nome: 'Ciclano',
-                partido: 'AFD',
-                vice: 'Cpf Cancelado',
+                partido: 'ABC',
+                vice: 'Cic',
                 fotos:[
                     {url:'99.jpg', legenda: 'Prefeito'},
-                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
                 ]
             },
             {
-                numero: 98,
-                nome: 'Chiquinho Host',
-                partido: 'SNN',
-                vice: 'NDN',
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
                 fotos:[
-                    {url: '84.jpg', legenda: 'Prefeito'},
-                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
-                ]        
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
             },
-        ]  
-    }   
-]
+        ]
+    }
+];
