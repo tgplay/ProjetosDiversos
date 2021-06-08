1db3665aa01e73c23d150740be00784228e8bfef
Tiago <tiagoalmeidasp@hotmail.com>
2021-06-08 19:15:01 -0300

    Projeto Urna Eletrônica

---
+++ ProjetosJS/urna-eletronica/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                nomero: 77222,
+                nome: 'Beltrano de Silva',
+                partido: 'ACJEF',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },            
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'AFD',
+                vice: 'Cpf Cancelado',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: 98,
+                nome: 'Chiquinho Host',
+                partido: 'SNN',
+                vice: 'NDN',
+                fotos:[
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]        
+            },
+        ]  
+    }   
+]
