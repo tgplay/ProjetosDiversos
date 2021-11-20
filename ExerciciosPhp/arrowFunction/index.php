d8f066d80f330dbcb8ac5dc3b5ae3f2ad255219e
tgplay <tiagoalmeidasp@hotmail.com>
2021-11-20 10:33:11 -0300

    funções de arredondamento

---
+++ ExerciciosPhp/arrowFunction/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$dizimo = fn($valor) => $valor * 0.1;
+
+//$somar = fn($n1,$n2) => $n1+$n2;
+
+echo $somar(6);
+
+//Arrow function a partir do PHP 7.4
---
+++ ExerciciosPhp/fnNativasMatematica/index.php
@@ -0,0 +1,42 @@
+<?php
+$numero = 2.7;
+
+/* echo floor($numero); arredonda para baixo
+
+ echo ceil($numero); arredonda para cima
+
+ echo round($numero); arredonda dependendo do número
+
+ echo round($numero, 2); No segundo parâmetro posso definir quantas casas decimais. O padrão é 0.
+ ----------------------------------------------------------
+ */
+/*
+-----------------------------------------------------------
+//Função aleatória
+
+$aleatorio = rand(3,9);
+
+echo $aleatorio;
+-----------------------------------------------------------
+*/
+/*
+-----------------------------------------------------------
+//Para saber qual é o número maior da lista.
+$lista = [1,4,9,8];
+
+echo max($lista);
+-----------------------------------------------------------
+*/
+/*
+-----------------------------------------------------------
+//Para saber qual é o número menor da lista.
+$lista = [1,4,9,8];
+
+echo min($lista);
+-----------------------------------------------------------
+*/
+
+
+
+
+
