5c58d19a2da985972cb229d0f3cb702c5dcbfb7f
Tiago <tiago@wvtodoz.com.br>
2022-02-14 17:49:59 -0300

    criando segundo tema de estudos no wordpress

---
+++ ProjetosWordpress/wordpress/wp-content/themes/segundotema/include/setup.php
@@ -0,0 +1,16 @@
+<?php
+function ts_theme_styles(){
+    wp_enqueue_style("bootstrap", get_template_directory_uri()."/vendor/bootstrap/css/bootstrap.min.css");
+    wp_enqueue_style("fontawsome", get_template_directory_uri()."/vendor/font-awesome/css/font-awesome.min.css");
+    wp_enqueue_style("googlelora", "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic");
+    wp_enqueue_style("googleopensans", "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800");
+    wp_enqueue_style("cleanblog", get_template_directory_uri()."css/clean-blog.min.css");
+
+    wp_enqueue_script("jquery", get_template_uri()."/vendor/jquery/jquery.min.js");
+    /*wp_enqueue_script("bootstrapjs", get_template_uri()."/vendor/bootstrap/js/bootstrap.bundle.min.js");
+    wp_enqueue_script("cleanblog_js", get_template_uri()."/js/clean-blog.min.js");*/
+
+}
+function ts_after_setup(){
+
+}
