dbca7212b2701634c1e394611a9fb699c6271c57
Tiago <tiago@wvtodoz.com.br>
2022-02-15 15:06:51 -0300

    site wp bootstrap

--- ProjetosWordpress/wordpress/wp-content/themes/segundotema/functions.php
+++ ProjetosWordpress/wordpress/wp-content/themes/segundotema/functions.php
@@ -1,8 +1,8 @@
 <?php
 //include
-require get_template_directory()."/include/setup.php";
+require get_template_directory().'/include/setup.php';
 
 //hooks
-add_action("wp_enqueue_script", "ts_theme_styles");
-add_action("after_setup_theme", "ts_after_setup");
+add_action('wp_enqueue_scripts', 'ts_theme_styles');
+add_action('after_setup_theme', 'ts_after_setup');
 
